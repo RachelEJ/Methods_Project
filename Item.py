748a65d4c3a73e37d0b3c14ae4908e00c139a127
msstateACosby <akc369@msstate.edu>
2021-11-30 12:48:45 -0600

    Added some functions

--- Item.py
+++ Item.py
@@ -5,3 +5,22 @@ class Item:
         self.price = price
         self.quantity = quantity
         self.description = description
+
+    def getName(self):
+        return self.name
+
+    def getSKU(self):
+        return self.sku
+    def getPrice(self):
+        return self.price
+
+    def getQuantity(self):
+        return self.quantity
+
+    def getDescription(self):
+        return self.description
+
+    def changeQuantity(self, quantity):
+        self.quantity = quantity
+
+        
--- User.py
+++ User.py
@@ -1,3 +1,5 @@
+import ShoppingCart, PurchaseHistory
+
 class User:
     def __init__(self, username, password, firstName, lastName, address, cardNumber):
         self.username = username
@@ -6,4 +8,27 @@ class User:
         self.lastName = lastName
         self.address = address
         self.cardNumber = cardNumber
+        self.purchaseHistory = []
+        #need to make someway to generate new ids
+        self.cart = ShoppingCart.ShoppingCart(0)
 
+    # need to add functionality
+    # how do we want to access the global inventory?
+    def checkout(self, cardNumber):
+        self.purchaseHistory.append(PurchaseHistory(0, self.cart))
+        self.cart = ShoppingCart.ShoppingCart(00)
+
+    def getHistory(self):
+        return self.purchaseHistory
+    
+    def changeAddresss(self, newAddress):
+        self.address = newAddress
+
+    def changePayment(self, cardNumber):
+        self.cardNumber = cardNumber
+
+    
+
+    
+
+            
