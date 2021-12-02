2a91832faa82055c2368a45fa1ccde2dbc4c13cc
RachelEJ <rej3713@gmail.com>
2021-12-01 18:42:34 -0600

    Update ShoppingCart.py

--- ShoppingCart.py
+++ ShoppingCart.py
@@ -3,24 +3,30 @@ class ShoppingCart:
         self.userId = userId
         self.items = []
         self.db = db
+
     def addItem(self, sku, quantity):
         #checks if item actually exists
         actualItem = self.db.getItemBySku(sku)
         if (actualItem == False or actualItem.quantity < quantity):
             return False
+        
         self.items.append((sku, quantity))
         self.db.addCartItem(self.userId, sku, quantity)
         return True
 
     def removeItem(self, sku):
         i = 0
+        remCheck = 0
         while (i < len(self.items)):
             if (self.items[i][0] == sku):
                 self.items.pop(i)
-                self.db.removeCartItem(self.user, sku)
+                self.db.removeCartItem(self.userId, sku)
+                remCheck += 1
             i += 1
-
-        
+        if (remCheck > 0):
+            return True
+        else:
+            return False
 
     def changeQuantity(self, sku, newQuantity):
         i = 0
